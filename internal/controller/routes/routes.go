5e77dbacbab59e031f71bdc36820245342956c83
juba <juliano.pedraca@gmail.com>
2025-03-15 10:34:33 -0300

    remove-int-from-search-line

--- internal/controller/routes/routes.go
+++ internal/controller/routes/routes.go
@@ -2,8 +2,6 @@ package routes
 
 import (
 	sptrans "julianopedraca/clean_architecture_go/external/sptrans_api"
-	"log/slog"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,11 +13,7 @@ func RegisterRoutes(server *gin.Engine) {
 		Authentication(c, api)
 	})
 	server.GET("/search-line", func(c *gin.Context) {
-		lineNameString := c.Query("lineName")
-		lineNameNumber, err := strconv.Atoi(lineNameString)
-		if err != nil {
-			slog.Error("Failed to convert string to integer", "error", err.Error())
-		}
-		SearchLine(c, api, lineNameNumber)
+		lineName := c.Query("lineName")
+		SearchLine(c, api, lineName)
 	})
 }
