2f06e7f13395a15f9a108a9d7193230af1d2ef87
juba <juliano.pedraca@gmail.com>
2025-03-15 15:07:44 -0300

    check-ci-block-merge-test

--- internal/controller/routes/search_line.go
+++ internal/controller/routes/search_line.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
 	apiInterface "julianopedraca/clean_architecture_go/interfaces"
@@ -10,6 +11,7 @@ import (
 
 func SearchLine(c *gin.Context, api apiInterface.SptransApiInterface, line string) {
 	respBody, err := api.SearchLine(line)
+	fmt.Println("🚀 ~ file: search_line.go ~ line 13 ~ funcSearchLine ~ respBody : ", respBody)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
