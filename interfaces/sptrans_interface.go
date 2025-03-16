ac4649f039dd28e6133067ec55302260467d39fd
juba <juliano.pedraca@gmail.com>
2025-03-16 13:44:40 -0300

    gix-json-format-response

--- interfaces/sptrans_interface.go
+++ interfaces/sptrans_interface.go
@@ -1,6 +1,16 @@
 package interfaces
 
+type BusLine struct {
+	Cl int32
+	Lc bool
+	Lt string
+	Sl int32
+	Tl int32
+	Tp string
+	Ts string
+}
+
 type SptransApiInterface interface {
 	Authentication() ([]byte, error)
-	SearchLine(line string) ([]byte, error)
+	SearchLine(line string) ([]BusLine, error)
 }
