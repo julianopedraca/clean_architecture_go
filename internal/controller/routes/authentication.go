c81b2a3aa4b346791357fe1e330300076268c2d2
juba <juliano.pedraca@gmail.com>
2025-03-18 17:20:12 -0300

    add-dockerfile-and-docker-compose

--- internal/controller/routes/authentication.go
+++ internal/controller/routes/authentication.go
@@ -1,21 +1,14 @@
 package routes
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	apiInterface "julianopedraca/clean_architecture_go/interfaces"
 )
 
 func Authentication(c *gin.Context, api apiInterface.SptransApiInterface) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	respBody, err := api.Authentication()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
