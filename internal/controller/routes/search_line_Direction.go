24a00095e5c9b83a215fa49389480637500c6fe7
juba <juliano.pedraca@gmail.com>
2025-03-26 18:52:59 -0300

    rename-interface

--- internal/controller/routes/search_line_Direction.go
+++ internal/controller/routes/search_line_Direction.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SearchLineDirection(c *gin.Context, api apiInterface.SptransApiInterface, line string, direction string) {
+func SearchLineDirection(c *gin.Context, api apiInterface.MobilityInterface, line string, direction string) {
 	respBody, err := api.SearchLineDirection(line, direction)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
